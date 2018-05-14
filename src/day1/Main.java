735f8145b3401ddcd5f3c37b54a19fe3227f2554
shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2018-05-14 07:03:37 +0530

    Added Generic Array Print Class

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -1,9 +1,26 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Assignments");
+		System.out.println("1.Print Generic Array");
+		
+		
+		
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			Integer[] arr1 = {1,2,3};
+			String[] arr2 = {"Hello","World"};
+			GenericArrayPrinter.printArray(arr1);
+			GenericArrayPrinter.printArray(arr2);
+		}
+		
+		sc.close();
 	}
 
 }
